713b5d6176d756c7446fb63c0841f16635196304
Eugen <lipen77777venk@mail.ru>
2022-07-21 15:57:06 +0400

    adding try and except

--- list_sum.py
+++ list_sum.py
@@ -1,8 +1,15 @@
 def calc(list_numbers):
-    i=0
-    for number in list_numbers:
-        i += int(number)
-    return i
+    i = 0
+    step = 0
+    try:
+        for number in list_numbers:
+            i += int(number)
+            step += 1
+    except ValueError:
+        return f" {step} index in the array is not correct"
+
+
+    return f"Sum of numbers {i}"
 
 
 def main():
@@ -10,7 +17,6 @@ def main():
     list_numbers = numbers.split(",")
     result = calc(list_numbers)
     print(result)
-
 
 
 if __name__ == "__main__":
